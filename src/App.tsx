e42d79c5a09d3ef821c37df5e199711a10e6630e
Danilo Mendes <danilo.mirandha@gmail.com>
2024-04-13 14:17:50 -0300

    feat: add components in App

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import TimersContextProvider from "./store/timers-context.tsx";
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
+
+function App() {
+  return (
+    <TimersContextProvider>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </TimersContextProvider>
+  );
+}
+
+export default App;
